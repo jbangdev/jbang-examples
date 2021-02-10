29279448c78e1b5a6ab234fd2d5dd33d2978f0eb
Max Rydahl Andersen <manderse@redhat.com>
2021-02-10 19:50:53 +0100

    clearer outline

--- examples/opencv_ball_tracker.java
+++ examples/opencv_ball_tracker.java
@@ -168,7 +168,7 @@ public class opencv_ball_tracker extends Application {
         if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
             // for each contour, display it in blue
             for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
-                Imgproc.drawContours(frame, contours, idx, new Scalar(0, 0, 255));
+                Imgproc.drawContours(frame, contours, idx, new Scalar(255, 0, 0),5);
             }
         }
 
