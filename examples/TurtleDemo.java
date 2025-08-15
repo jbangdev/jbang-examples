9efcc5e97410eab5693ea9799bf312f3378ba707
Werner Fouché <werner.fouche@gmail.com>
2025-08-15 19:40:03 +0200

    fix: reformatted source code

--- examples/TurtleDemo.java
+++ examples/TurtleDemo.java
@@ -3,19 +3,16 @@
 
 import java.awt.Color;
 
-public class TurtleDemo
-{
-    public static void main(String[] args) 
-    {
+public class TurtleDemo {
+    public static void main(String[] args) {
         Turtle joe = new Turtle();
         joe.penColor(Color.RED);
-        for(int i = 0; i < 10; i++)
-        {
-               for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 4; j++) {
                 joe.forward(150);
                 joe.right(90);
-               }
-               joe.left(36);
+            }
+            joe.left(36);
         }
         joe.hide();
     }
