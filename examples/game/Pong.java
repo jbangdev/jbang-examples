7a9e7c04dd55542a753d1e6bf71456cce6018f1e
Werner Fouché <werner.fouche@gmail.com>
2025-08-19 15:54:30 +0200

    fix: added support for macos aarch64 (arm64)

--- examples/game/Pong.java
+++ examples/game/Pong.java
@@ -1,16 +1,24 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
+
 //DEPS org.lwjgl:lwjgl:3.3.6
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6
+
 //DEPS org.lwjgl:lwjgl:3.3.6:natives-windows
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-windows
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-windows
+
 //DEPS org.lwjgl:lwjgl:3.3.6:natives-linux
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-linux
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-linux
+
 //DEPS org.lwjgl:lwjgl:3.3.6:natives-macos
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-macos
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-macos
+
+//DEPS org.lwjgl:lwjgl:3.3.6:natives-macos-arm64
+//DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-macos-arm64
+//DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-macos-arm64
 
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
