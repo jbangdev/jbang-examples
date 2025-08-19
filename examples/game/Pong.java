9cea2cb79f1a086495f554a5baf9cb7283d5c9a7
Werner Fouché <werner.fouche@gmail.com>
2025-08-19 16:01:48 +0200

    fix: added support for linux aarch64 (arm64)

--- examples/game/Pong.java
+++ examples/game/Pong.java
@@ -16,6 +16,10 @@
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-linux
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-linux
 
+//DEPS org.lwjgl:lwjgl:3.3.6:natives-linux-arm64
+//DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-linux-arm64
+//DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-linux-arm64
+
 //DEPS org.lwjgl:lwjgl:3.3.6:natives-macos
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-macos
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-macos
