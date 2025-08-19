98125b2dee1aa1a1312276c41add1c219e34aee9
Werner Fouché <werner.fouche@gmail.com>
2025-08-19 15:58:58 +0200

    fix: added support for windows aarch64 (arm64)

--- examples/game/Pong.java
+++ examples/game/Pong.java
@@ -8,6 +8,10 @@
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-windows
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-windows
 
+//DEPS org.lwjgl:lwjgl:3.3.6:natives-windows-arm64
+//DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-windows-arm64
+//DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-windows-arm64
+
 //DEPS org.lwjgl:lwjgl:3.3.6:natives-linux
 //DEPS org.lwjgl:lwjgl-glfw:3.3.6:natives-linux
 //DEPS org.lwjgl:lwjgl-opengl:3.3.6:natives-linux
